0a04a948a14d4fea8c70e1a0fafb096b8c8cc5b2
Konstantinos Tsirakos <tkonstantinos1996@hotmail.com>
2019-11-20 15:31:16 +0200

    Created number next to vote box graph, with icons and all according to array on the smarttable component

--- fullstack-template/frontend/src/app/smarttable/smarttable.component.ts
+++ fullstack-template/frontend/src/app/smarttable/smarttable.component.ts
@@ -10,19 +10,20 @@ export class SmarttableComponent implements OnInit {
   constructor() { }
 
   players = [
-    {"width" : "59%" , "color" : "red" , "name" : "DeadPool" , "img" : "assets/avatars/deadpool.png" , "votes" : 4},
-    {"width" : "50%" , "color" : "yellow" , "name" : "Picachu" , "img" : "assets/avatars/picachu.png" , "votes" : 4},
-    {"width" : "50%" , "color" : "orange" , "name" : "Iron Man" , "img" : "assets/avatars/ironman.png" , "votes" : 4},
-    {"width" : "50%" , "color" : "red" , "name" : "Spiderman" , "img" : "assets/avatars/spiderman.jpg" , "votes" : 4},
-    {"width" : "50%" , "color" : "brown" , "name" : "Bear" , "img" : "assets/avatars/bear.png" , "votes" : 4},
-    {"width" : "30%" , "color" : "blue" , "name" : "Spongebob" , "img" : "assets/avatars/spongbob.png" , "votes" : 4},
-    {"width" : "20%" , "color" : "brown" , "name" : "Bear" , "img" : "assets/avatars/bear.png" , "votes" : 4},
-    {"width" : "10%" , "color" : "blue" , "name" : "Spongebob" , "img" : "assets/avatars/spongbob.png" , "votes" : 4},    
-    {"width" : "0%" , "color" : "brown" , "name" : "Bear" , "img" : "assets/avatars/bear.png" , "votes" : 4},
-    {"width" : "0%" , "color" : "blue" , "name" : "Spongebob" , "img" : "assets/avatars/spongbob.png" , "votes" : 4},
+    {"width" : "40vw" , "color" : "red" , "name" : "DeadPool" , "img" : "assets/avatars/deadpool.png" , "votes" : 4},
+    {"width" : "30vw" , "color" : "yellow" , "name" : "Picachu" , "img" : "assets/avatars/picachu.png" , "votes" : 3},
+    {"width" : "20vw" , "color" : "orange" , "name" : "Iron Man" , "img" : "assets/avatars/ironman.png" , "votes" : 2},
+    {"width" : "10vw" , "color" : "red" , "name" : "Spiderman" , "img" : "assets/avatars/spiderman.jpg" , "votes" : 1},
+    {"width" : "0vw" , "color" : "brown" , "name" : "Bear" , "img" : "assets/avatars/bear.png" , "votes" : 0},
+    {"width" : "0vw" , "color" : "blue" , "name" : "Spongebob" , "img" : "assets/avatars/spongbob.png" , "votes" : 0},
+    {"width" : "0vw" , "color" : "brown" , "name" : "Bear" , "img" : "assets/avatars/bear.png" , "votes" : 0},
 
   ]
   ngOnInit() {
+
+
+
+
   }
 
 }
