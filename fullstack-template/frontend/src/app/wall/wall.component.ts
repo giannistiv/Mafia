525d49173d4b775a6369653ecb0febfb0a21da08
csd3582 <csd3582@csd.uoc.gr>
2019-12-19 13:32:37 +0200

    Wall ready

--- fullstack-template/frontend/src/app/wall/wall.component.ts
+++ fullstack-template/frontend/src/app/wall/wall.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WallComponent implements OnInit {
 
+  public night: boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.night= false;
+  }
+  
+  chnight(){
+    this.night=!this.night;
   }
 
 }
