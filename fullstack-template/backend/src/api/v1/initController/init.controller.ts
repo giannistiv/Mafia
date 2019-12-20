91bfa067d4445b6b46826ee514507d3eb14bc0f0
csd3582 <csd3582@csd.uoc.gr>
2019-12-20 02:39:37 +0200

    Fixed descriptions

--- fullstack-template/backend/src/api/v1/initController/init.controller.ts
+++ fullstack-template/backend/src/api/v1/initController/init.controller.ts
@@ -43,32 +43,47 @@ export class InitController {
         {"name" : "Detective" ,  "counter" : 0 , "toBeAssigned" : 2 , "role" : {
             "name" : "Detective",
             "img" : "assets/roles/detective.png",
-            "info" : "someinfo",
-            "ability" : "Τhe Detective can detect whether a player is a mafioso or not once every round"
+            "info" : "You are a veteran cop in your last year on the force. You have been hunting the Mafia for years \
+            but they were always one step ahead. But not this time. It is up to you and the people of this town to capture \
+            the Mafia once and for all.",
+            "ability" : "Τhe Detective knows the all the mafiosi except for the Godfather. Try to hint on the killers but beware \
+            if they find you you will be the first to go."
         }},
         {"name" : "Doctor" ,  "counter" : 0 , "toBeAssigned" : 1 , "role" : {
             "name" : "Doctor",
             "img" : "assets/roles/doctor.png",
-            "info" : "someinfo",
-            "ability" : "The Doctor can save a random person during the night, if the mafia killed him"
+            "info" : "There are always victims and there will be many more. But a job is a job and you will save \
+            anyone that is trustworthy. Until the Mafia gets extinguished from this town, you will have to heal the \
+            whomever you can and believe in, even yourself.",
+            "ability" : "The Doctor can save someone during the night, if the mafia killed him, even himself. You have \
+            this ability once so use it carefully."
         }},
         {"name" : "Masons" ,  "counter" : 0 , "toBeAssigned" : 3 , "role" : {
             "name" : "Masones",
             "img" : "assets/roles/masons.png",
-            "info" : "Masons can recognize each other.",
-            "ability" : "Masons can recognize each other."
+            "info" : "Masons are secretive and hidden in the shadows. They blend in the crowds, hidden by their anonymity \
+            and trusting only their own. But this anonimity may have to be lost in order to help this town in the upcoming \
+            battle",
+            "ability" : "Masons know each other. They seem like pawns in this grand game of chess, but with their \
+            knowledge they know more than anyone else"
         }},
         {"name" : "Barman" ,  "counter" : 0 , "toBeAssigned" : 2 , "role" : {
             "name" : "Barman",
             "img" : "assets/roles/barman.png",
-            "info" : "some info",
-            "ability" : "The Barman may anonymously cancel the effect of another role's ability every night"
+            "info" : "Barmen are a quit bunch. They hear the customers rumble on and on revealing even their most hidden \
+            secrets. But this calm and collected exterior is not but a mask for people with the patience to learn all \
+            about a person's past. And they know just the person to sell their secrets.",
+            "ability" : "The Barman is a mafioso. They know the other members of the Mafia, but which of them is the Godfather? \
+            Even they don't know."
         }},
         {"name" : "Godfather" ,  "counter" : 0 , "toBeAssigned" : 1, "role" : {
             "name" : "Godfather",
             "img" : "assets/roles/godfather.png",
-            "info" : "SomeInfo",
-            "ability" : "The Godfather will be identified by the detective as innocent"
+            "info" : "You are the one king of this town. You have brought it to greatness and you will do everything to \
+            keep it there. But this achievement may crumble if this Detective and the other vermin of this town \
+            continue to search into your empire. It is time to assemble you team and finish them once and for all.",
+            "ability" : "The Godfather remains hidden from the Detective. He knows the other Mafiosi and has to rely on them \
+            to win the game. But if the people start to suspect him, he will have to drop the blame to someone else, even to one of his own"
         }},
     ]
 
